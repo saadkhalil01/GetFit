58725b7ba42b4ccb64f59c54f3069b5f0ef981cf
saadkhalil01 <bscf20a525@pucit.edu.pk>
2023-11-05 18:38:47 +0500

    Firebase Setup

--- App.jsx
+++ App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import Navigation from './src/navigation'
-import Practice from './src/screens/auth/Practice'
 
 const App = () => {
   return (
     <>
-      <Practice />
+      <Navigation />
     </>
   )
 }
